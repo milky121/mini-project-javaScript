cf8c6041a29d0c9534561ed0c59e244390517705
milky121 <milkyrani8@gmail.com>
2024-03-28 09:58:06 +0530

    delete perform in list

--- toto.js
+++ toto.js
@@ -15,5 +15,23 @@ btn.addEventListener("click", function(){
     inp.value = "";
 });
 
+// ul.addEventListener("click",function(event){
+//     if(event.target.nodeName == "BUTTON"){
+//         let listItem = event.target.parentElement;
+//         listItem.remove();
+//         console.log("deleted");
+//     }
+// });
+
 
 let delBtns = document.querySelectorAll(".delete");
+for(delBtn of delBtns){
+    delBtn.addEventListener("click" , function(){
+        let par = this.parentElement;
+        console.log(par);
+        par.remove();
+
+    });
+}
+
+// EVENT DELEGATION
